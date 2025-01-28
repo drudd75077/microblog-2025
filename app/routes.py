436b27fe0958d8989a5982a36aac52df048ec430
drudd <drudd75077@gmail.com>
2025-01-28 00:28:01 -0600

    added login logic to route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
+from urllib.parse import urlsplit
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+import sqlalchemy as sa
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -22,9 +26,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',  title='Sign In', form=form)

b95b683cc02fada01b0f8ee0a88a529838e79f7b
drudd <drudd75077@gmail.com>
2025-01-27 23:43:14 -0600

    adding password information

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
@@ -18,7 +19,10 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)

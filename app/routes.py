67a3c01d0a90cab4d924401d82124e55aef545be
drudd <drudd75077@gmail.com>
2025-01-28 22:59:01 -0600

    testing the login page

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -21,7 +22,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -40,3 +41,10 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html',  title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+

581dd5a84dd30ea6bf612e586036e0234ac08599
drudd <drudd75077@gmail.com>
2025-02-01 01:43:14 -0600

    edit profit page 2 not file error

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
 import os
+from PIL import Image
+import secrets
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
-
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+        
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -74,5 +82,38 @@ def user(username):
         {'author':user, 'body':'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts, image_file=image_file)
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(16)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        if form.image_file.data:
+            picture_file = save_picture(form.image_file.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+        
+
 
 

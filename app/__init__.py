bd06e9fde638d0d3e24e5df960422717b6195e99
drudd <drudd75077@gmail.com>
2025-02-05 02:16:12 -0600

    can't figure out sending an email

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -15,21 +17,43 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-os.getcwd()
+    class SendGridEmailHandler(logging.Handler):
+        def __init__(self, api_key, to_email):
+            super().__init__()
+            self.api_key = api_key
+            self.to_email = to_email
+
+        def emit(self, record):
+            message = self.format(record)
+            self.send_email(message)
+
+        def send_email(self, message):
+            sg = SendGridAPIClient(self.api_key)
+            email = Mail(
+                from_email=app.config['MAIL_DEFAULT_SENDER'],
+                to_emails=self.to_email,
+                subject='Error Log Notification',
+                plain_text_content=message
+            )
+            try:
+                sg.send(email)
+            except Exception as e:
+                print(f"Failed to send email: {e}")
+            
+# Configure logging
+logger = logging.getLogger('EmailLogger')
+logger.setLevel(logging.ERROR)
+
+# Set up SendGrid email handler
+api_key = app.config['SENDGRID_API_KEY']
+email_handler = SendGridEmailHandler(api_key, app.config['SENDGRID_RECIPIENTS'])
+print('email handler', vars(email_handler))
+formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+email_handler.setFormatter(formatter)
+logger.addHandler(email_handler)
+
+#sending log to text file
 if not os.path.exists('logs'):
     os.mkdir('logs')
 file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
@@ -38,8 +62,6 @@ file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
-
 app.logger.setLevel(logging.INFO)
-app.logger.info('Microblog startup')
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['david@letsworkapps.com']
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    SENDGRID_RECIPIENTS = os.environ.get('RECIPIENTS')
+    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')

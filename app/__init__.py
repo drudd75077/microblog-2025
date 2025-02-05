de3b2e917406e1b52a7bc51a8f6d5172a7f6b372
drudd <drudd75077@gmail.com>
2025-02-05 02:21:06 -0600

    updating error level to see if that's an issue

--- app/__init__.py
+++ app/__init__.py
@@ -62,6 +62,6 @@ file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 file_handler.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.INFO)
+app.logger.setLevel(logging.ERROR)
 
 from app import routes, models, errors
